7ad408bd6939a4f52d4fb400fbb27115ea418b90
Juha-Matti Santala <juha-matti.santala@futurice.com>
2020-04-18 09:48:06 +0300

    Automatically hide new entries
    Youtube adds new videos when you scroll the page down. This improvement
    automatically blurs old videos as they are added to the view when scrolling.

--- content.js
+++ content.js
@@ -1,10 +1,47 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  var observeDOM = (function () {
+    var MutationObserver =
+      window.MutationObserver || window.WebKitMutationObserver;
+
+    return function (obj, callback) {
+      if (!obj || !obj.nodeType === 1) return; // validation
+
+      if (MutationObserver) {
+        // define a new observer
+        var obs = new MutationObserver(function (mutations, observer) {
+          callback(mutations);
+        });
+        // have the observer observe foo for changes in children
+        obs.observe(obj, { childList: true, subtree: true });
+      } else if (window.addEventListener) {
+        obj.addEventListener("DOMNodeInserted", callback, false);
+        obj.addEventListener("DOMNodeRemoved", callback, false);
+      }
+    };
+  })();
+
   if (request.message === "clicked_browser_action") {
+    observeDOM(document.getElementsByTagName("body")[0], () => {
+      const videoItems = Object.values(
+        document.querySelectorAll("#content")
+      ).filter((node) => node.classList.contains("ytd-rich-item-renderer"));
+
+      videoItems.forEach((videoNode) => {
+        const metaBlock = videoNode.querySelector("#metadata-line");
+        const dateBlock = metaBlock?.children[1];
+
+        if (dateBlock?.innerText?.includes("year")) {
+          console.log("found one");
+          videoNode.style.filter = "grayscale(1) blur(7.4px)";
+        }
+      });
+    });
+
     const videoItems = Object.values(
       document.querySelectorAll("#content")
-    ).filter(node => node.classList.contains("ytd-rich-item-renderer"));
+    ).filter((node) => node.classList.contains("ytd-rich-item-renderer"));
 
-    videoItems.forEach(videoNode => {
+    videoItems.forEach((videoNode) => {
       const metaBlock = videoNode.querySelector("#metadata-line");
       const dateBlock = metaBlock?.children[1];
 
